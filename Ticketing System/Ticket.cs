be7a1bf564ad9af1e510c270e6958b837c917cef
LittleURF <hubertzachlowski@gmail.com>
2019-04-20 20:16:35 +0200

    Setting up DB Connection

---
+++ Ticketing System/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ticketing_System
+{
+    public class Ticket
+    {
+        public int TicketID { get; set; }
+        public int CreatorID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool IsFinished { get; set; }
+
+
+        public void DisplayTicket()
+        {
+            Console.WriteLine($"Ticket ID: {TicketID}, Creator ID: {CreatorID}\n{Title}\n{Description}\n{CreationDate}");
+        }
+    }
+}
---
+++ Ticketing System/TicketsDB.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace Ticketing_System
+{
+    public class TicketsDB
+    {
+        private readonly SqlConnection _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Tickets"].ConnectionString);
+
+    }
+}
